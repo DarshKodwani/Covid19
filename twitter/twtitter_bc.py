7e0885c3452a932b6b0083ee0c7cefc2046ddc1b
tharshi <tsworks@protonmail.ch>
2020-04-18 15:35:10 -0400

    update for loss and accuracy plots + regularization

--- twitter/twtitter_bc.py
+++ twitter/twtitter_bc.py
@@ -35,13 +35,13 @@ np.random.seed(27)
 
 # control modules, full run needs all of them on if you want to
 # recalculate everything
-import_data = False
+import_data = True
 need_tokens = False
 need_features = False
-need_model = True
-train = True
-test_k = True
-cluster = True
+need_model = False
+train = False
+test_k = False
+cluster = False
 viz = True
 
 if import_data:
@@ -55,15 +55,12 @@ if import_data:
                   "followers_count",
                   "place_type", "created_at"], axis=1)
     
-    df = df.sample(frac=1)
+    df = df.sample(frac=0.25)
     
     print("Cleaning data...")
     df = df.dropna(axis=0)
     df = df.reset_index(drop=True)
     print("Done.")
-
-
-
 #%% CLEAN DATA
 if need_tokens:
     print("Tokenizing data...")
@@ -96,6 +93,7 @@ if need_tokens:
     tokens_re = re.compile(r'('+'|'.join(regex_str)+')', re.VERBOSE | re.IGNORECASE)
     emoticon_re = re.compile(r'^'+emoticons_str+'$', re.VERBOSE | re.IGNORECASE)
     
+    # create stemmer
     ps = PorterStemmer()
     
     def tokenise(s):
@@ -132,13 +130,15 @@ if need_tokens:
         df.at[i, 'tokens'] = tokens
     
     # save tokens
+    print("Saving token data...")
     with open("tokens.txt", "wb") as fp:
         pickle.dump(tokenised, fp)
-    
     print("Done.")
 else:
+    print("Loading token data...")
     with open("tokens.txt", "rb") as fp:
         tokenised = pickle.load(fp)
+    print("Done.")
 
 #%% Build features from clean data
 if need_features:
@@ -164,12 +164,15 @@ if need_features:
     
     print("Extracting features from tokenized words...")
     X = tfidf.fit_transform(tokenised)
+    print("Saving feature matrix...")
     sp.sparse.save_npz("data_mat.npz", X)
     del tokenised
     del tweets
     print('Done. The data matrix has shape {}'.format(X.shape))
 else:
+    print("Loading feature matrix...")
     X = sp.sparse.load_npz("data_mat.npz")
+    print("Done.")
 
 #%% Data Viz - PCA
 
@@ -193,7 +196,7 @@ else:
 # ax1.set_yticklabels([])
 # ax1.set_xticklabels([])
 # ax1.grid('False')
-# plt.savefig("pca_embedding.png", dpi=600)
+# plt.savefig("pca_embedding.png", dpi=300)
 #%% Data Viz - tSNE
 
 # tsne = TSNE(n_components=2)
@@ -210,7 +213,7 @@ else:
 # ax2.set_yticklabels([])
 # ax2.set_xticklabels([])
 # ax2.grid('False')
-# plt.savefig("tnse_embedding.png", dpi=600)
+# plt.savefig("tnse_embedding.png", dpi=300)
 
 #%% Let's define an autoencoder to do the heavy lifiting since
 # SVD and tSNE take way too long
@@ -219,57 +222,122 @@ if need_model:
     print("Initializing model...")
     from keras.layers import Input, Dense
     from keras.models import Model
+    from keras import regularizers
+    from keras.callbacks import EarlyStopping
     
     n_epochs = 10
     batch_size = X.shape[0] // 100
+    reg = 1e-4
+    split = 0.30 
+    pat = 5
+    
     N1 = X.shape[1] // 100
-    N2 = max(N1 // 10, 100)
+    N2 = N1 // 10
     N3 = max(N2 // 10, 10)
     
     # define autoencoder
     input_img= Input(shape=(X.shape[-1],))
-    encoded = Dense(units=N1, activation='relu')(input_img)
-    encoded = Dense(units=N2, activation='relu')(encoded)
-    encoded = Dense(units=N3, activation='relu')(encoded)
-    decoded = Dense(units=N2, activation='relu')(encoded)
-    decoded = Dense(units=N1, activation='relu')(decoded)
-    decoded = Dense(units=X.shape[-1], activation='sigmoid')(decoded)
+    encoded = Dense(units=N1, activation='relu'
+                    , kernel_regularizer=regularizers.l2(reg))(input_img)
+    encoded = Dense(units=N2, activation='relu'
+                    , kernel_regularizer=regularizers.l2(reg))(encoded)
+    encoded = Dense(units=N3, activation='relu'
+                    , kernel_regularizer=regularizers.l2(reg))(encoded)
+    decoded = Dense(units=N2, activation='relu'
+                    , kernel_regularizer=regularizers.l2(reg))(encoded)
+    decoded = Dense(units=N1, activation='relu'
+                    , kernel_regularizer=regularizers.l2(reg))(decoded)
+    decoded = Dense(units=X.shape[-1], activation='sigmoid'
+                    , kernel_regularizer=regularizers.l2(reg))(decoded)
     autoencoder=Model(input_img, decoded)
     encoder = Model(input_img, encoded)
-    
-    # print summary
-    autoencoder.summary()
     
     # compile AE
     autoencoder.compile(optimizer='adam', 
                         loss='binary_crossentropy', 
                         metrics=['accuracy'])
+    
+    # print summary
+    autoencoder.summary()
     print("Done.")
 
     if not train:
+        print("Loading AE weights...")
         autoencoder.load_weights('ae_weights.h5')
+        print("Done.")
     else:
-        autoencoder.fit(X, X, 
-                        epochs=n_epochs, 
-                        batch_size=batch_size, 
-                        shuffle=True)
-        autoencoder.save_weights('ae_weights.h5')
-    
+        fit_params = {"batch_size": batch_size,
+                      "epochs": n_epochs, 
+                      "verbose": True,
+                      "validation_split": split,
+                      "shuffle": True,
+                      "callbacks": [EarlyStopping(verbose=True,
+                                                  patience=pat,
+                                                  monitor='val_loss')]}
+            
+        print("Training model...")
+        history = autoencoder.fit(X, X, **fit_params)
+        print("Done.")
 
+#%% Save and plot training info
+
+if train:
+        # plot acc and loss
+        fig4, ax4 = plt.subplots()
+        ax4.plot(history.history['accuracy'], label='train')
+        ax4.plot(history.history['val_accuracy'], label='validation')
+        ax4.set_title("Model Accuracy")
+        plt.xlabel("epochs")
+        plt.ylabel("accuracy")
+        plt.legend()
+        plt.savefig("acc_history.png", dpi=300)
+        
+        fig5, ax5 = plt.subplots()
+        ax5.plot(history.history['loss'], label='train')
+        ax5.plot(history.history['val_loss'], label='validation')
+        ax5.set_title("Model Loss")
+        plt.xlabel("epochs")
+        plt.ylabel("loss")
+        plt.legend()
+        plt.savefig("loss_history.png", dpi=300)
+        
+        # save data
+        print("Saving AE weights...")
+        autoencoder.save_weights('ae_weights.h5')
+        print("Done.")
+        
+        print("Saving compressed data...")
+        compressed_data = encoder.predict(X)
+        np.save("encoded_data.npy", compressed_data)
+        print("Done.")
+        
 #%% K-Means clustering - choosing K part 1
 
 if test_k:
     
-    compressed_data = encoder.predict(X)
-    np.save('comp_data.npy', compressed_data)
+    if 'compressed_data' in locals():
+        pass
+    else:
+        print("Loading encoded data...")
+        compressed_data = np.load('encoded_data.npy')
+        print("Done.")
     
     print("Running inertia analysis...")
     # figure out what K is good for the data set
     km_inertias = []
     
-    num_k = max(round(0.01 * compressed_data.shape[0]), 10)
+    # determine a reasonable number of K to test
+    def num_k_trials(n):
+        if n <= 10:
+            return n
+        elif n > int(4.5e5):
+            return round((4.5e5 * 2**((10 - (4.5e5) / ((4.5e5)**0.785)))))
+        else:
+            return round(n * 2**((10 - n) / n**0.785))
+        
+    num_k = num_k_trials(compressed_data.shape[0])
     print("{} values of K to test.".format(num_k))
-    k_range = range(1, num_k)
+    k_range = range(1, num_k + 1)
     for k in k_range:
         # find clusters for given k
         km = KMeans(n_clusters=k)
@@ -279,11 +347,14 @@ if test_k:
         inertia = km.inertia_
         km_inertias.append(inertia)
         print("k = {} ... inertia = {}".format(k, inertia))
-
+        
+    
     km_inertias = np.array(km_inertias)
+    print("Done.")
 else:
-    compressed_data = np.load('comp_data.npy')
-
+    print("Loading compressed data...")
+    compressed_data = np.load('encoded_data.npy')
+    print("Done.")
 #%% K-Means clustering - choosing K part 2
 
 if test_k:
@@ -291,6 +362,7 @@ if test_k:
     kneedle = KneeLocator(k_range, km_inertias, S=1.0,
                           curve='convex', direction='decreasing')
     K = round(kneedle.elbow)
+    print("Saving K and K-Means labels...")
     np.save('k_val.npy', K)
     np.save('km_labels.npy', km.labels_)
     print("Done.")
@@ -301,15 +373,17 @@ if test_k:
     plt.ylabel("K-Means Inertia")
     plt.xlabel("K")
     plt.title("Inertia Plot for K-Means Algorithm")
-    plt.savefig("inertia.png", dpi=600)
+    plt.savefig("inertia.png", dpi=300)
 else:
+    print("Loading K and K-Means labels...")
     K = np.load('k_val.npy')
     labels = np.load('km_labels.npy')
+    print("Done.")
     
 #%% Final analysis with chosen K
 
 # cluster
-if cluster:
+if cluster and import_data:
     print("Clustering Tweets...")
     km = KMeans(n_clusters=K)
     km = km.fit(compressed_data)
@@ -321,24 +395,25 @@ if cluster:
 else:
     df = pd.read_pickle("labeled_tweets")
 
+#%% Plot 2D grid of all compressed dimensions
 if viz:
     from mpl_toolkits.mplot3d import Axes3D
-    import itertools
     
     if 'compressed_data' in locals():
-        Z = compressed_data
+        pass
     else:
-        compressed_data = np.load('comp_data.npy')
+        print("Loading compressed data...")
+        compressed_data = np.load('encoded_data.npy')
+        print("Done.")
         
-    n_dim = Z.shape[1]
+    n_dim = compressed_data.shape[1]
     
     if 'labels' in locals():
         pass
     else:
+        print("Loading K-Means labels...")
         labels = np.load('km_labels.npy')
-    
-    for comb in itertools.combinations(range(n_dim), 3):
-        print(comb)
+        print("Done.")
     
     # define the colormap
     cmap = plt.cm.jet
@@ -352,17 +427,17 @@ if viz:
     norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
         
     # make the scatterplot
-    fig3 = plt.figure()
-    ax3 = fig3.add_subplot(111, projection='3d')
-    scat = ax3.scatter(Z[:, 0], Z[:, 1], Z[:, 2], 
-                       c=labels, cmap=cmap,
-                       norm=norm, s=6)
+    fig3, ax3 = plt.subplots()
+    scat = ax3.scatter(compressed_data[:, 0],
+                        compressed_data[:, 5],
+                        c=labels, cmap=cmap,
+                        norm=norm, s=6)
     # create the colorbar
     cb = plt.colorbar(scat, spacing='proportional', ticks=bounds)
     cb.set_label('Classes')
     ax3.set_title('Tweets - First 3 axes of AE with class labels')
     ax3.set_yticklabels([])
     ax3.set_xticklabels([])
-    plt.savefig("AE_embedding_km.png", dpi=600)
+    plt.savefig("AE_embedding_km.png", dpi=300)
 
 print("\nProgram exit.")
